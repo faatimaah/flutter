649ee2d10cbfd5ee7f80e582373a8ad0965060d0
Fathima <85221316+faatimaah@users.noreply.github.com>
2021-06-10 23:16:39 +0530

    Add files via upload

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter_application_1/widgets/top_var.dart';
+import 'package:flutter/material.dart';
+
+class HomepageScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TopBar(
+                title: 'Fathima',
+                subtitle: 'Developer',
+                color: Color(0xff0B3D2E),
+              ),
+              const SizedBox(height: 10),
+              new Container(
+                width: 100,
+                height: 100,
+                alignment: Alignment.center,
+                margin: const EdgeInsets.all(15.0),
+                padding: const EdgeInsets.all(3.0),
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.blueAccent)),
+                child: Text("About me"),
+              ),
+              new Container(
+                width: 100,
+                height: 100,
+                alignment: Alignment.center,
+                margin: const EdgeInsets.all(15.0),
+                padding: const EdgeInsets.all(3.0),
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.blueAccent)),
+                child: Text("Social Media"),
+              ),
+              new Container(
+                width: 100,
+                height: 100,
+                alignment: Alignment.center,
+                margin: const EdgeInsets.all(15.0),
+                padding: const EdgeInsets.all(3.0),
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.blueAccent)),
+                child: Text("Hobbies"),
+              ),
+              new Container(
+                width: 100,
+                height: 100,
+                alignment: Alignment.center,
+                margin: const EdgeInsets.all(15.0),
+                padding: const EdgeInsets.all(3.0),
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.blueAccent)),
+                child: Text("Experience"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
